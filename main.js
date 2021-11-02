8b370aa1f57fc15a5a8d1194b8f0cbff1dea6345
Areo Bnejamen <areotimileyin1@gmail.com>
2021-11-02 16:57:38 +0100

    responsiveness initiated

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-//script
-const elect = document.getElementById("after-main");
+function firstList() {
+    document.getElementById("drop1").classList.toggle("show");
+}
+
+function secondList() {
+    document.getElementById("drop2").classList.toggle("show");
+}
+
+function thirdList() {
+    document.getElementById("drop3").classList.toggle("show");
+}
+
+function fourthList() {
+    document.getElementById("drop4").classList.toggle("show");
+}
+
+window.onmouseenter = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
