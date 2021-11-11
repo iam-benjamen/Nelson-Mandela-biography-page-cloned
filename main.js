f74681aa9e52fe179721e003e2f5656b157c39ca
Areo Bnejamen <areotimileyin1@gmail.com>
2021-11-11 09:52:56 +0100

    dropdowns fixed

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-function firstList() {
-  document.getElementById("drop1").classList.toggle("show");
-}
+// function firstList() {
+//   document.getElementById("drop1").classList.toggle("show");
+// }
 
-function secondList() {
-  document.getElementById("drop2").classList.toggle("show");
-}
+// function secondList() {
+//   document.getElementById("drop2").classList.toggle("show");
+// }
 
-function thirdList() {
-  document.getElementById("drop3").classList.toggle("show");
-}
+// function thirdList() {
+//   document.getElementById("drop3").classList.toggle("show");
+// }
 
-function fourthList() {
-  document.getElementById("drop4").classList.toggle("show");
-}
+// function fourthList() {
+//   document.getElementById("drop4").classList.toggle("show");
+// }
 
-window.onmouseleave = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// window.onmouseleave = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
